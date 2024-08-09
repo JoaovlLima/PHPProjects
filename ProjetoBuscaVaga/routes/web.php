aef9b37f21bbd0559ac5c2ca8b1a98c1c4b8a6e4
JoaovlLima <jl949615@gmail.com>
2024-08-09 16:58:03 -0300

    09.08.24

--- ProjetoBuscaVaga/routes/web.php
+++ ProjetoBuscaVaga/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [UsuarioController::class, 'showLoginForm'])->
+name('usuarios.login');
+
+Route::get('/login', [UsuarioController::class, 'login'])->
+name('usuarios.login');
+
+// Rota para exibir o formulário de registro
+Route::get('/registro', [UsuarioController::class, 'showRegisterForm'])->
+name('usuarios.register');
+
+// Rota para processar o registro
+Route::post('/registro', [UsuarioController::class, 'register'])->
+name('usuarios.register');
+
+// Rota para logout
+Route::post('/logout', [UsuarioController::class, 'logout'])->
+name('usuarios.logout');
+
+// Rota para o dashboard, protegida por autenticação
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
+
+
